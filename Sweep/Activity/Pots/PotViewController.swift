bac878b5b3d5b613c75d068e0160b767849b0a6d
Paul Williamson <squarefrog@gmail.com>
2019-03-04 15:39:58 +0000

    Create paged scroll view layout for Pot display

--- Sweep/Activity/Pots/PotViewController.swift
+++ Sweep/Activity/Pots/PotViewController.swift
@@ -8,17 +8,30 @@ class PotViewController: UICollectionViewController, Storyboarded {
     private let viewModel = PotViewModel()
     private var dataSource: DataSource<Pot>!
 
+    /// Create a dummy scroll view to make paging the collection view easier
+    private lazy var pagingScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.isHidden = true
+        scrollView.delegate = self
+        scrollView.isPagingEnabled = true
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+
+    /// Keep a reference to the paging scroll view's left and right constraints, as they will need
+    /// to be updated whenever the view is laid out
+    private var scrollViewLeftConstraint: NSLayoutConstraint!
+    private var scrollViewRightConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         assert(dataSource != nil, "display() must be called")
 
         registerCells()
-    }
-
-    private func registerCells() {
-        collectionView.register(viewModel.cellClass,
-                                forCellWithReuseIdentifier: viewModel.reuseIdentifier)
+        addPagingScrollView()
+        layoutPagingScrollView()
+        setupPagingPanGestureRecognizer()
     }
 
     /// Sets up a collection view data source to display pots
@@ -35,4 +48,52 @@ class PotViewController: UICollectionViewController, Storyboarded {
                                      configureCell: viewModel.configure)
     }
 
+    private func registerCells() {
+        collectionView.register(viewModel.cellClass, forCellWithReuseIdentifier: viewModel.reuseIdentifier)
+    }
+
+    /// After the layout has been prepared, update the scroll views content size and constraints
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        guard let layout = collectionViewLayout as? PeekPagedFlowLayout else { return }
+
+        pagingScrollView.contentSize = layout.collectionViewContentSize
+
+        // Make the scroll view the same width and offset of a cell plus its spacing
+        scrollViewLeftConstraint.constant = layout.sectionInset.left
+        scrollViewRightConstraint.constant = -layout.spacing
+    }
+
+    private func addPagingScrollView() {
+        view.addSubview(pagingScrollView)
+    }
+
+    private func layoutPagingScrollView() {
+        // Save a reference to the left and right edge constraints for use later
+        scrollViewLeftConstraint = pagingScrollView.leftAnchor.constraint(equalTo: view.leftAnchor)
+        scrollViewRightConstraint = pagingScrollView.rightAnchor.constraint(equalTo: view.rightAnchor)
+
+        let constraints: [NSLayoutConstraint] = [
+            scrollViewLeftConstraint,
+            scrollViewRightConstraint,
+            pagingScrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            pagingScrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ]
+
+        NSLayoutConstraint.activate(constraints)
+    }
+
+    /// In order to gain the benefit of the paged scroll view, we must tell collection view to use
+    /// the paged scroll view's pan gesture recognizer
+    private func setupPagingPanGestureRecognizer() {
+        collectionView.addGestureRecognizer(pagingScrollView.panGestureRecognizer)
+        collectionView.panGestureRecognizer.isEnabled = false
+    }
+
+    /// As the scroll view scrolls, update the collection view's content offset
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard scrollView == pagingScrollView else { return }
+        collectionView.contentOffset = scrollView.contentOffset
+    }
 }
--- Sweep/Layout/PeekPagedFlowLayout.swift
+++ Sweep/Layout/PeekPagedFlowLayout.swift
@@ -7,7 +7,7 @@ import UIKit
 /// A custom collection view flow layout that allows peeking at the next item
 class PeekPagedFlowLayout: UICollectionViewFlowLayout {
     /// The amount of interitem spacing
-    let spacing: CGFloat = 30.0
+    let spacing: CGFloat = 20.0
 
     /// If the bounds change, then the layout should be invalidated
     override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
@@ -19,13 +19,16 @@ class PeekPagedFlowLayout: UICollectionViewFlowLayout {
 
         guard let collectionView = collectionView else { return }
 
-        // Calculate the available width for the items
-        let availableWidth = collectionView.bounds.inset(by: collectionView.layoutMargins).width
-        let width = availableWidth - spacing * 2
-
-        itemSize = CGSize(width: width, height: collectionView.bounds.height)
-        sectionInset = UIEdgeInsets(top: 0, left: spacing, bottom: 0, right: spacing)
         minimumLineSpacing = spacing
-    }
 
+        // How much should the next cell peek into the view
+        let peekAmount = spacing * 4
+
+        // Calculate the available width for the items
+        let width = collectionView.bounds.width - peekAmount
+        itemSize = CGSize(width: width, height: collectionView.bounds.height)
+
+        // Add left inset to center the cell
+        sectionInset = UIEdgeInsets(top: 0, left: spacing * 2, bottom: 0, right: 0)
+    }
 }
