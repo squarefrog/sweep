d1d5b724fa9141d106589f689be9866fbc307978
Paul Williamson <squarefrog@gmail.com>
2019-03-12 22:42:02 +0000

    Configure account view model

--- Sweep/Activity/Accounts/AccountsViewModel.swift
+++ Sweep/Activity/Accounts/AccountsViewModel.swift
@@ -5,27 +5,22 @@
 import UIKit
 
 /// A view model responsible for configuring how the `AccountsViewController` displays data
-struct AccountsViewModel: CardCellPresentable, ProvidesBackgroundColors {
-    /// Copy the background colors so they are not reshuffled each time they are used
-    private let backgroundColors = shuffledBackgroundColors
-
+struct AccountsViewModel: AccountCellPresentable {
     func configure(cell: UICollectionViewCell, for account: Account, item: Int) {
-        guard let cell = cell as? CardCell else { return }
+        guard let cell = cell as? AccountCell else { return }
         configureLabel(in: cell, for: account)
-        configureBackground(in: cell, for: item)
     }
 
-    private func configureLabel(in cell: CardCell, for account: Account) {
-        cell.mainLabel.text = account.description
-        cell.mainLabel.textColor = Theme.textColor.color
-        cell.mainLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+    private func configureLabel(in cell: AccountCell, for account: Account) {
+        let viewModel = AccountBalanceViewModel(account: account)
+        cell.balanceLabel.textColor = Theme.textColor.color
+        cell.balanceLabel.attributedText = viewModel.formattedBalance()
+
+        cell.descriptionLabel.textColor = Theme.textColor.color
+        cell.descriptionLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+        cell.descriptionLabel.text = account.description
     }
 
-    private func configureBackground(in cell: CardCell, for index: Int) {
-        cell.backgroundColor = backgroundColors[index].color
-    }
-
-    /// Configure view
     func setupView(_ view: PagedCollectionView) {
         view.backgroundColor = Theme.backgroundColor.color
         registerCardCell(in: view.collectionView)
